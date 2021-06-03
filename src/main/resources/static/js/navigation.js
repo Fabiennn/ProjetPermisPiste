bfecb0700d376cfcb428aea18942a9d100ed8eac
Félix <felix.geffrault@gmail.com>
2021-06-03 18:43:43 +0200

    Same

---
+++ src/main/resources/static/js/navigation.js
@@ -0,0 +1,21 @@
+
+function toggleDropDown(event){
+    const button = event.target;
+    const subMenu = button.nextElementSibling;
+    subMenu.classList.toggle("active");
+    console.log(subMenu.classList.contains('active'))
+    if(subMenu.classList.contains('active')){
+        const wrapperHeight = subMenu.firstElementChild.offsetHeight;
+        subMenu.style.height = `${wrapperHeight}px`;
+    }else{
+        subMenu.style.height = `0`;
+    }
+}
+
+document.querySelectorAll(".drop-title").forEach(button => {
+    button.onclick = toggleDropDown;
+})
+
+function toggleNavBar(){
+    document.querySelector(".navbar2-nav").classList.toggle('active');
+}
